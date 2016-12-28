4d893d2aea6ff8ef23a44dcc1982f0b525462040
tomboyo <tomasimmons@gmail.com>
2016-12-27 23:21:23 -0500

    Update/Flesh-out Game Tree logic

--- src/main/java/edu/vwc/mastermind/tree/GameTree.java
+++ src/main/java/edu/vwc/mastermind/tree/GameTree.java
@@ -1,11 +1,35 @@
 package edu.vwc.mastermind.tree;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import edu.vwc.mastermind.sequence.Code;
+import edu.vwc.mastermind.sequence.Response;
+
 public class GameTree {
+	
+	public final Code guess;
+	public final Map<Response, List<Code>> answerGroups;
+	
+	private List<GameTree> children;
+	
+	public GameTree(Code guess, Map<Response, List<Code>> answerGroups) {
+		this.guess = guess;
+		this.answerGroups = answerGroups;
+		this.children = new LinkedList<GameTree>();
+	}
+	
+	public void addChild(GameTree child) {
+		children.add(child);
+	}
 
 	public GameTreeVisitor<?> accept(GameTreeVisitor<?> visitor) {
 		visitor.visit(this);
 		
-		// Then visit each child here
+		for (GameTree child : children) {
+			visitor.visit(child);
+		}
 		
 		return visitor;
 	}
