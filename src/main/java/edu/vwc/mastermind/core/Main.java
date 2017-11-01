f450ffe45ca61013a959dd8244fe86b6c979b393
Tom Simmons <tomasimmons@gmail.com>
2017-10-31 21:55:42 -0400

    Update configuration to match refactoring

--- src/main/java/edu/vwc/mastermind/core/Main.java
+++ src/main/java/edu/vwc/mastermind/core/Main.java
@@ -1,30 +1,54 @@
 package edu.vwc.mastermind.core;
 
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import edu.vwc.mastermind.sequence.Code;
+import edu.vwc.mastermind.sequence.provider.CodesProviderFactory;
 import edu.vwc.mastermind.tree.Tree;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		String configuration = System.getProperty("edu.vwc.mastermind.config");
+		if (args.length == 0 || args[0].isEmpty())
+			throw new IllegalArgumentException(
+					"missing path argument to configuration file");
 		
-		if (configuration.isEmpty()) {
-			System.err.println("No configuration file provided."
-					+ " Set with -Dedu.vwc.mastermind.config=\"path/to/xml\"");
-		}
+		String configuration = args[0];
 		System.out.println("Attempting to load configuration from "
 				+ configuration);
 		
 		FileSystemXmlApplicationContext context = 
 				new FileSystemXmlApplicationContext(configuration);
 		
-		Controller controller = context.getBean("controller", Controller.class);
+		//Controller controller = context.getBean("controller", Controller.class);
+		
+		Comparator<Tree> comparator =
+				context.getBean("comparator", Comparator.class);
+		ExecutorService executor =
+				context.getBean("executor", ExecutorService.class);
+		CodesProviderFactory guessProviderFactory =
+				context.getBean("factory", CodesProviderFactory.class);
+		Set<Code> firstGuesses = context.getBean("first_guesses", Set.class);
+		Set<Code> allAnswers = context.getBean("all_answers", Set.class);
+		
+		TreeFactory treeFactory = new TreeFactory(
+				comparator,
+				guessProviderFactory);
+		ParallelizedTreeFactory parallelizedTreeFactory =
+				new ParallelizedTreeFactory(executor, treeFactory);
+		Controller controller = new Controller(
+				parallelizedTreeFactory,
+				comparator);
 		
 		try {
-			Tree tree = controller.newOptimizedStrategyTree();
+			Tree tree = controller.newOptimizedStrategyTree(
+					firstGuesses, allAnswers);
 			System.out.println("Startegy tree maximum turns: " + tree.depth());
 			System.out.println(tree);
 		} catch (ExecutionException | InterruptedException e) {
