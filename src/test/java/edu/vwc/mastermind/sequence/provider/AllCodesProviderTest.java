a367f875da8f4d511462005f8b2f5dffa5dd8940
Tom Simmons <tomasimmons@gmail.com>
2017-11-24 22:35:59 -0500

    Use test helper to create set

--- src/test/java/edu/vwc/mastermind/sequence/provider/AllCodesProviderTest.java
+++ src/test/java/edu/vwc/mastermind/sequence/provider/AllCodesProviderTest.java
@@ -1,5 +1,6 @@
 package edu.vwc.mastermind.sequence.provider;
 
+import static edu.vwc.mastermind.TestHelper.setOfCodes;
 import static java.util.stream.Collectors.toSet;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
@@ -9,6 +10,7 @@ import java.util.stream.Stream;
 
 import org.junit.Test;
 
+import edu.vwc.mastermind.TestHelper;
 import edu.vwc.mastermind.sequence.Code;
 import edu.vwc.mastermind.sequence.provider.AllCodesProvider;
 
@@ -21,11 +23,10 @@ public class AllCodesProviderTest {
 		provider = new AllCodesProvider(3, 2);
 		
 		Set<Code> actual = provider.getCodes();
-		Set<Code> expected = Stream.of(
+		Set<Code> expected = setOfCodes(
 				Code.valueOf(0, 0), Code.valueOf(0, 1), Code.valueOf(0, 2),
 				Code.valueOf(1, 0), Code.valueOf(1, 1), Code.valueOf(1, 2),
-				Code.valueOf(2, 0), Code.valueOf(2, 1), Code.valueOf(2, 2))
-		.collect(toSet());
+				Code.valueOf(2, 0), Code.valueOf(2, 1), Code.valueOf(2, 2));
 		
 		assertThat(actual, equalTo(expected));
 	}
