a1a16a0df506f5ef166291b4448a0907cbda26f6
Tom Simmons <tomasimmons@gmail.com>
2018-02-19 19:47:07 -0500

    Update greedy codes provider to emulate Knuth.
    This allows us to emulate the results of Knuth's approach, which finds
    a solution in 5 turns to the standard game of 6 colors and 4 pegs.

--- src/main/java/edu/vwc/mastermind/sequence/provider/GreedyCodesProviderFactory.java
+++ src/main/java/edu/vwc/mastermind/sequence/provider/GreedyCodesProviderFactory.java
@@ -12,12 +12,24 @@ import java.util.stream.IntStream;
 import edu.vwc.mastermind.sequence.Code;
 import edu.vwc.mastermind.sequence.Response;
 
+/**
+ * This code provider strategy emulates that of Donald Knuth. We choose
+ * exactly one code to use as the next guess. We pick the code as the next
+ * guess which guarantees a maximal number of remaining answers will be
+ * eliminated. We prefer to pick codes which are themselves possible answers.
+ * We consider each code from the universe as a candidate.
+ *
+ * This algorithm will guarantee a 5-turn game in 6 colors and 4 pegs using
+ * the starting input [1, 1, 0, 0].
+ */
 public final class GreedyCodesProviderFactory implements CodesProviderFactory {
 
 	private final Collection<Code> universe;
+	private final int sizeOfUniverse;
 	
 	public GreedyCodesProviderFactory(Collection<Code> universe) {
 		this.universe = universe;
+		sizeOfUniverse = universe.size();
 	}
 	
 	@Override
@@ -26,26 +38,29 @@ public final class GreedyCodesProviderFactory implements CodesProviderFactory {
 			Set<Code> answersRemaining) {
 		if (answersRemaining.isEmpty())
 			throw new NoSuchElementException("There are no answers left");
-		
-		Set<Code> remainingGuesses = new LinkedHashSet<Code>(universe);
-		remainingGuesses.removeAll(alreadyGuessed);
-		if (remainingGuesses.isEmpty())
-			throw new NoSuchElementException(
-					"There are no guesses left to make");
-		
-		Iterator<Code> iter = remainingGuesses.iterator();
+
+		if (alreadyGuessed.equals(universe))
+			throw new NoSuchElementException("There are no guesses left");
+
+		Iterator<Code> iter = universe.iterator();
 		Code bestGuess = iter.next();
-		int largestBin = largestBin(getBins(bestGuess, answersRemaining));
-		
+		int bestScore = score(getBins(bestGuess, answersRemaining));
+		boolean guessIsAnswer = answersRemaining.contains(bestGuess);
+
 		while (iter.hasNext()) {
 			Code next = iter.next();
-			int localLargestBin = largestBin(getBins(next, answersRemaining));
-			if (localLargestBin < largestBin) {
-				largestBin = localLargestBin;
+			int score = score(getBins(next, answersRemaining));
+			if (score > bestScore) {
+				bestScore = score;
 				bestGuess = next;
+				guessIsAnswer = answersRemaining.contains(bestGuess);
+			} else if (score == bestScore && !guessIsAnswer) {
+				bestScore = score;
+				bestGuess = next;
+				guessIsAnswer = true;
 			}
 		}
-		
+
 		Set<Code> codes = new LinkedHashSet<>();
 		codes.add(bestGuess);
 		CodesProvider provider = new SimpleCodesProvider(codes);
@@ -66,11 +81,12 @@ public final class GreedyCodesProviderFactory implements CodesProviderFactory {
 		return bins;
 	}
 	
-	private int largestBin(Map<Response, CountingNumber> bins) {
-		return bins.values().stream()
+	private int score(
+			Map<Response, CountingNumber> bins) {
+		return sizeOfUniverse - bins.values().stream()
 				.flatMapToInt(it -> IntStream.of(it.get()))
 				.max()
-				.orElse(0);
+				.orElse(0); // This should not occur
 	}
 	
 	private static class CountingNumber {
--- src/test/java/edu/vwc/mastermind/sequence/provider/GreedyCodesProviderFactoryTest.java
+++ src/test/java/edu/vwc/mastermind/sequence/provider/GreedyCodesProviderFactoryTest.java
@@ -1,7 +1,9 @@
 package edu.vwc.mastermind.sequence.provider;
 
 import static edu.vwc.mastermind.TestHelper.setOfCodes;
+import static java.util.Collections.emptySet;
 import static java.util.stream.Collectors.toSet;
+import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.junit.Assert.*;
@@ -14,6 +16,7 @@ import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,97 +25,72 @@ import edu.vwc.mastermind.sequence.Code;
 public class GreedyCodesProviderFactoryTest {
 
 	GreedyCodesProviderFactory factory;
-	Collection<Code> universe;
-	
-	@Before
-	public void setUp() {
-		universe = new HashSet<>();
-		factory = new GreedyCodesProviderFactory(universe);
-	}
-	
-	@Test
-	public void onlyRemainingGuessIsReturned() {
-		universe.addAll(setOfCodes(
-				Code.valueOf(0, 0), Code.valueOf(0, 1),
-				Code.valueOf(1, 0), Code.valueOf(1, 1),
-				Code.valueOf(2, 3)));
-		
-		Set<Code> answersRemaining = new HashSet<>(universe);
-		
-		for (Code remainingCode : universe) {
-			Set<Code> alreadyGuessed = newSetOfAllCodesExcept(
-					universe, remainingCode);
-			testOnlyRemainingGuessIsAlwaysReturned(
-					universe,
-					answersRemaining,
-					alreadyGuessed);
-		}
-	}
-	
-	private Set<Code> newSetOfAllCodesExcept(
-			Collection<Code> all, Code except) {
-		return all.stream()
-				.filter(code -> !code.equals(except))
-				.collect(toSet());
-	}
-	
-	private void testOnlyRemainingGuessIsAlwaysReturned(
-			Collection<Code> universe,
-			Set<Code> answersRemaining,
-			Set<Code> alreadyGuessed) {
-		Collection<Code> copyOfUniverse = new HashSet<>(universe);
-		copyOfUniverse.removeAll(alreadyGuessed);
-		assumeThat(copyOfUniverse.size(), equalTo(1));
-		
-		Code remaining = copyOfUniverse.iterator().next();
-		
-		Collection<Code> result = factory
-				.getInstance(alreadyGuessed, answersRemaining)
-				.getCodes();
-		assertThat(result.size(), equalTo(1));
-		assertThat(result, hasItem(remaining));
-	}
-	
+
 	@Test(expected = NoSuchElementException.class)
 	public void noGuessesRemaining() {
-		universe = setOfCodes(Code.valueOf(1));
+		Set<Code> universe = setOfCodes(Code.valueOf(1));
 		Set<Code> alreadyGuessed = new HashSet<>(universe);
 		Set<Code> answersRemaining = setOfCodes(Code.valueOf(2));
 
+		factory = new GreedyCodesProviderFactory(universe);
+
 		factory.getInstance(alreadyGuessed, answersRemaining);
 	}
-	
+
 	@Test(expected = NoSuchElementException.class)
 	public void noAnswersLeft() {
-		universe = setOfCodes(Code.valueOf(1));
-		Set<Code> alreadyGuessed = Collections.emptySet();
-		Set<Code> answersRemaining = Collections.emptySet();
-		
+		Set<Code> universe = setOfCodes(Code.valueOf(1));
+		Set<Code> alreadyGuessed = emptySet();
+		Set<Code> answersRemaining = emptySet();
+
+		factory = new GreedyCodesProviderFactory(universe);
+
 		factory.getInstance(alreadyGuessed, answersRemaining);
 	}
-	
+
 	@Test
-	public void preferGuessesWithSmallestPeakAnswerGroupSize() {
-		Code worseChoice = Code.valueOf(1, 1);
-		Code betterChoice = Code.valueOf(2, 3);
-		
-		// All remaining answers are lumped under the response (0, 0, 2) when
-		// the worse choice is guessed. When the better choice is guessed, they
-		// are divided between (1, 0, 1) and (0, 1, 1), so there are fewer
-		// answers per response group.
-		universe.addAll(setOfCodes(
-				worseChoice, betterChoice,
-				Code.valueOf(2, 2), Code.valueOf(3, 3)));
-		
-		Set<Code> alreadyGuessed = universe.stream()
-				.filter(code -> !code.equals(worseChoice)
-						&& !code.equals(betterChoice))
-				.collect(toSet());
-		Set<Code> answersRemaining = new HashSet<>(alreadyGuessed);
-		
-		Set<Code> actual = factory
-				.getInstance(alreadyGuessed, answersRemaining)
+	public void maximizeMinimumNumberOfEliminatedAnswers() {
+		Set<Code> universe = setOfCodes(
+				Code.valueOf(1, 1, 1),
+				Code.valueOf(1, 2, 1),
+				Code.valueOf(1, 2, 3));
+		Set<Code> alreadyGuessed = emptySet();
+		Set<Code> answersRemaining = universe;
+
+		factory = new GreedyCodesProviderFactory(universe);
+
+		Collection<Code> codes = factory.getInstance(
+				alreadyGuessed,
+				answersRemaining)
 				.getCodes();
-		assertThat(actual, equalTo(setOfCodes(betterChoice)));
+
+		// [1, 1, 1] and [1, 2, 3] always eliminate two possibilities.
+		// [1, 2, 1] Might only eliminate one, as when it receives the
+		// feedback [2, 0, 1].
+		assertThat(codes, anyOf(
+				hasItem(Code.valueOf(1, 1, 1)),
+				hasItem(Code.valueOf(1, 2, 3))));
+	}
+
+	@Test
+	public void preferPossibleAnswersAsGuessesDuringTies() {
+		Set<Code> universe = setOfCodes(
+				Code.valueOf(1, 1, 1),
+				Code.valueOf(1, 2, 1),
+				Code.valueOf(1, 2, 3));
+		Set<Code> alreadyGuessed = emptySet();
+		Set<Code> answersRemaining = setOfCodes(
+				Code.valueOf(1, 1, 1),
+				Code.valueOf(1, 2, 1));
+
+		factory = new GreedyCodesProviderFactory(universe);
+
+		Collection<Code> codes = factory.getInstance(alreadyGuessed,
+				answersRemaining)
+				.getCodes();
+
+		// [1, 1, 1] and [1, 2, 3] are the best choices; only [1, 1, 1] is a
+		// possible answer, so we prefer that.
+		assertThat(codes, hasItem(Code.valueOf(1, 1, 1)));
 	}
 }
